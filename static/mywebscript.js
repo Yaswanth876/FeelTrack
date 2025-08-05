bc91eceb194ed820d5c0f41817f87f9c3eefbb5d
Yaswanth876 <vsyaswanth008@gmail.com>
2025-08-05 16:31:23 +0530

    modified

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -14,16 +14,17 @@ let RunSentimentAnalysis = () => {
                 try {
                     const result = JSON.parse(xhttp.responseText);
 
+                    const emotionScores = result.emotion_scores || {};
+                    let emotionsList = "";
+
+                    for (const [emotion, score] of Object.entries(emotionScores)) {
+                        emotionsList += `<li>${emotion.charAt(0).toUpperCase() + emotion.slice(1)}: ${score.toFixed(3)}</li>`;
+                    }
+
                     const responseHTML = `
                         <div class="card mt-4 p-3">
                             <h4><strong>Dominant Emotion:</strong> ${result.dominant_emotion?.toUpperCase()}</h4>
-                            <ul>
-                                <li>Anger: ${result.anger.toFixed(3)}</li>
-                                <li>Disgust: ${result.disgust.toFixed(3)}</li>
-                                <li>Fear: ${result.fear.toFixed(3)}</li>
-                                <li>Joy: ${result.joy.toFixed(3)}</li>
-                                <li>Sadness: ${result.sadness.toFixed(3)}</li>
-                            </ul>
+                            <ul>${emotionsList}</ul>
                         </div>`;
                     
                     document.getElementById("system_response").innerHTML = responseHTML;
