7d380798eaa44d9a3a57c4a810adf6bd96b18f0f
Yaswanth876 <vsyaswanth008@gmail.com>
2025-08-05 17:57:17 +0530

    modified

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,13 +4,13 @@ from transformers import pipeline
 emotion_classifier = pipeline("text-classification", model="boltuix/bert-emotion", return_all_scores=True)
 
 def emotion_detector(text):
-    results = emotion_classifier(text)[0]  # It's a list with dicts of label & score
+    results = emotion_classifier(text)[0]  # List of dicts: {'label': ..., 'score': ...}
     emotion_scores = {res["label"]: round(res["score"], 3) for res in results}
-    
+
     # Get top emotion
     dominant_emotion = max(emotion_scores, key=emotion_scores.get)
 
     return {
-        "emotion": dominant_emotion,
+        "dominant_emotion": dominant_emotion,  # <-- IMPORTANT: use this key
         "emotion_scores": emotion_scores
     }
